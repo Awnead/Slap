32208b8ca871d503f5270660a8df24c2860fd3f1
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-07-03 10:16:56 -0400

    added ChatHeader

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Main from './Main'
-import Sidebar from './Sidebar'
-import Chat from './Chat'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
-        <Sidebar />
-        <Chat />
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react'
 
+import Sidebar from './Sidebar'
+import ChatHeader from './ChatHeader'
+import Chat from './Chat'
+
 class Main extends Component {
     render() {
         return (
-            <div className="Main">
-                <h1>Slap</h1>
+            <div className="Main" style={styles}>
+                <Sidebar />
+                <ChatHeader />
+                <Chat />
             </div>
         )
     }
+}
+
+const styles = {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
 }
 
 export default Main
