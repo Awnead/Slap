b05fd65c2afc77b9a1e89c99436f1258dd2fe93a
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-07-03 09:48:59 -0400

    Added Chat and Sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Main from './Main'
+import Sidebar from './Sidebar'
+import Chat from './Chat'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+        <Sidebar />
+        <Chat />
       </div>
     );
   }
