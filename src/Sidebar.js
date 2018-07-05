9391fcc4f05d30887566f01717827d640cba1270
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-07-05 15:38:13 -0400

    Added RoomList

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,29 +1,19 @@
 import React from 'react'
 
 import UserInfo from './UserInfo'
+import RoomList from './RoomList'
 
 const Sidebar= (props) => {
     return (
-        <div className="Sidebar">
-            <aside className="Sidebar" style = {styles.Sidebar}>
-                <div className="UserInfo" style={styles.Children}>
-                    <div className="Avatar"></div>
-                    <div className="user">
-                        <UserInfo user={props.user}/>
-                    </div>
-                </div>
-                <h1 style={styles.h1}>XTBC 18</h1>
-                <nav className="RoomList" style={styles.Children}>
-                    <h2>Rooms</h2>
-                    <ul>
-                    <li><a href="#">general</a></li>
-                    <li><a href="#">random</a></li>
-                    </ul>
-                </nav>
-            </aside>
 
-            
-        </div>
+        <aside
+         className="Sidebar"
+         style = {styles.Sidebar}
+        >
+            <UserInfo user={props.user}/>
+            <h1 style={styles.h1}>XTBC 18</h1>
+            <RoomList />
+        </aside>
     )
 }
 
@@ -36,10 +26,7 @@ const styles = {
         display: 'flex',
         flexDirection: 'column',
     },
-      
-    Children: {
-        padding: '0 1rem',
-    },
+    
       
     h1: {
         color: 'white',
