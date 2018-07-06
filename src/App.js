c54ea7e2df67de04cfae04727c5946af676dde5e
Andrew Nead <andrewnead@andrews-mbp-2.coldspring.marian.edu>
2018-07-06 00:34:35 -0400

    Implimented authentication work flow.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Main from './Main'
-
+import SignIn from './SignIn'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      user:{
-        uid: 'Awnead',
-        displayName:'Airpilot17',
-        email: 'airpilot17@frontier.com',
-      }
+      user:{}
     }
   }
+
+  handleAuth = (user) => {
+    this.setState({ user })
+  }
+
+  signedIn = () => {
+    return (this.state.user.uid)
+  }
+
+  signOut = () => {
+    this.setState({user: {} })
+  }
+
   render() {
     return (
       <div className="App">
-        <Main user={this.state.user}/>
+        {
+          this.signedIn()
+            ? <Main
+                user={this.state.user}
+                signOut={this.signOut}
+              />
+            : <SignIn handleAuth={this.handleAuth} />
+        }
       </div>
     );
   }
